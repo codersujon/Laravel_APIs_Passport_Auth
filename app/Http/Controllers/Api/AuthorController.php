52739f632fbaf6b806956eab71a849a12541b7d9
MD. SUJAN AHMED <csesujon155@gmail.com>
2025-04-18 16:43:50 -0700

    Setup API Controllers (Author & Book)

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Register API (POST, formdata)
+     */
+    public function register(Request $request){
+
+    }
+
+    /**
+     * Login API (POST, formdata)
+     */
+    public function login(Request $request){
+
+    }
+
+    /**
+     * Profile API (GET)
+     */
+    public function profile(){
+
+    }
+
+     /**
+     * Logout API (GET)
+     */
+    public function logout(){
+
+    }
+}
---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Create Book API (POST, formdata)
+     */
+    public function saveBook(Request $request){
+
+    }
+
+    /**
+     * List Book API (GET)
+     */
+    public function listBooks(){
+
+    }
+
+    /**
+     * Delete Book API (DELETE)
+     */
+    public function deleteBook($bookId){
+
+    }
+}
